f8405d632dea2f0d2b4e4345f62ed3b7f56e13a4
nihei1206 <nihei1206@gmail.com>
2022-11-30 14:42:06 +0700

    &lr=lang_jaに変更

--- app/background/background.js
+++ app/background/background.js
@@ -8,19 +8,19 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 
   if(urlManager.isGoogle(tab.url)) {
     if(statusManager.getStatus()) {
-      chrome.tabs.update(tab.id, {url:tab.url + "&tbs=qdr:y"});
+      chrome.tabs.update(tab.id, {url:tab.url + "&lr=lang_ja"});
     } else {
       var url = tab.url;
-      url = url.replace(/&as_qdr=y1/g, "");
-      url = url.replace(/&tbs=qdr:y/g, "");
+      url = url.replace(/&lr=lang_ja/g, "");
+      url = url.replace(/&lr=lang_ja/g, "");
       chrome.tabs.update(tab.id, {url:url});
     }
   }
 });
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if(statusManager.getStatus()) {
-    if(urlManager.isGoogle(tab.url) && !urlManager.getParam(tab.url, "tbs") && !urlManager.getParam(tab.url, "url")) {
-      chrome.tabs.update(tabId, {url:tab.url + "&tbs=qdr:y"});
+    if(urlManager.isGoogle(tab.url) && !urlManager.getParam(tab.url, "ls") && !urlManager.getParam(tab.url, "url")) {
+      chrome.tabs.update(tabId, {url:tab.url + "&lr=lang_ja"});
     }
   }
 });
